2eb2abf844c14504c6408b83dc2739b3511a1f23
annakispert <annakispert@gmail.com>
2024-07-01 23:37:56 -0400

    Update bananaGame.ts

--- bananaGame.ts
+++ bananaGame.ts
@@ -1,1 +1,9 @@
+const pullCardBtn = document.getElementById("pullCard");
+const cardTxt = document.getElementById("cardText");
+
+pullCardBtn.addEventListener("click", function() {
+  cardTxt.innerHTML = "Hello";
+});
+
+
 
